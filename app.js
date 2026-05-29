63865f3d590da32aec42d40df7a6458f3d7c0d69
leandroyuyu <leandro.7549@colegioprogresso.g12.br>
2026-05-29 07:10:43 -0300

    Finalizado

--- app.js
+++ app.js
@@ -4,18 +4,28 @@ const display = document.getElementById("display");
 
 function inserirTarefa() {
    let tarefa = input.value;
+
+   if(tarefa == ''){
+    alert('Favor inserir uma tarefa')
+    return
+   }
+
    display.innerHTML +=
     `<div class="tarefa">
-      ${tarefa} 
+      ${tarefa}
          <div>
-            <button class="delete"> Deletar </button> 
+            <button class="delete"> Deletar </button>
             <button class="edit"> Editar </button>
-         <div> 
+         <div>
     </div>`;
    input.value = ''
 }
 
+function removerTarefa(event){
+    if(event.target.classList.contains('delete')){
+        event.target.parentElement.parentElement.remove()
+    }
+}
 
-button.addEventListener("click", inserirTarefa)      
-
-
+button.addEventListener("click", inserirTarefa)
+display.addEventListener("click", removerTarefa)
